3f587662be9826bd375c559bf1845f1a25576a75
Azfar Sulaiman <azfar.sulaiman@gmail.com>
2024-03-03 23:33:25 -0500

    Sunday Mar 3

--- src/main.py
+++ src/main.py
@@ -13,12 +13,19 @@ def str2bool(v):
 
 def config():
     parser = argparse.ArgumentParser(description='Process prompt search agent arguments')
+    # Remains the same
     parser.add_argument('--task_name', type=str, default='bigbench',  help='This is consistent to the task file names in tasks folder. The default is bigbench task.')  
+    # Remains the same
     parser.add_argument('--search_algo', type=str, default='mcts', choices=['mcts', 'beam'], help='Prompt search algorithm. Choose from \'mcts\' and \'beam\'.')    
+    # Remains the same
     parser.add_argument('--batch_size', type=int, default=5, help='Batch size depending on the memory and model size')
+    # Remains the same
     parser.add_argument('--depth_limit', type=int, default=5, help="The max depth of a single searching path.")
+    # Train size - will change
     parser.add_argument('--train_size', type=int, default=None, help="The dataset that sample batches from.")
+
     parser.add_argument('--eval_size', type=int, default=50, help="Calculate reward on this set.")
+    # Test set size - will not change
     parser.add_argument('--test_size', type=int, default=0, help="Test set size.")
     parser.add_argument('--seed', type=int, default=42, help="The seed to shuffle the dataset.")
     parser.add_argument('--train_shuffle', type=str2bool, default=True, help='Shuffle training set')
@@ -43,7 +50,7 @@ def config():
     # Others
     parser.add_argument('--log_dir', type=str, default='../logs/', help='Log directory.')
     parser.add_argument('--data_dir', type=str, default=None, help='Path to the data file (if needed)')
-    parser.add_argument('--api_key', type=str, default=None, help='OpenAI api key or PaLM 2 api key')
+    parser.add_argument('--api_key', type=str, default=os.getenv("OPENAI_API_KEY"), help='OpenAI api key or PaLM 2 api key')
     
     # BeamSearch
     parser.add_argument('--beam_width', type=int, default=3)
